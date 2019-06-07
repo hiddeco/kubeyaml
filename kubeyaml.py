d292b13f67a32e4fded2fadbca65a16b9fc61b9a
Hidde Beydals <hello@hidde.co>
2019-06-07 18:28:31 +0200

    Make kubeyaml understand image path annotations

--- kubeyaml.py
+++ kubeyaml.py
@@ -9,6 +9,12 @@ from ruamel.yaml import YAML
 # FluxHelmRelease
 FHR_CONTAINER = 'chart-image'
 
+# Annotations used in a FluxHelmRelease to map paths to image values in
+# dot notation
+REGISTRY_ANNOTATION = 'registry.flux.weave.works/'
+REPOSITORY_ANNOTATION = 'repository.flux.weave.works/'
+TAG_ANNOTATION = 'tag.flux.weave.works/'
+
 class NotFound(Exception):
     pass
 
@@ -163,9 +169,47 @@ def set_container_image(manifest, container, image):
 def mappings(values):
     return ((k, values[k]) for k in values if isinstance(values[k], collections.Mapping))
 
+def container_mappings_from_annotations(manifest):
+    def update(d, u):
+        for k, v in u.items():
+            if isinstance(v, collections.Mapping):
+                d[k] = update(d.get(k, {}), v)
+            else:
+                d[k] = v
+        return d
+
+    mappings = {}
+    try:
+        annotations = manifest['metadata']['annotations']
+        for k, v in annotations.items():
+            if k.startswith(REGISTRY_ANNOTATION):
+                name = k[len(REGISTRY_ANNOTATION):]
+                update(mappings, {name: {'registry': v}})
+            if k.startswith(REPOSITORY_ANNOTATION):
+                name = k[len(REPOSITORY_ANNOTATION):]
+                update(mappings, {name: {'repository': v}})
+                continue
+            if k.startswith(TAG_ANNOTATION):
+                name = k[len(TAG_ANNOTATION):]
+                update(mappings, {name: {'tag': v}})
+                continue
+    except KeyError:
+        pass
+    return mappings
+
 # There are different ways of interpreting FluxHelmRelease values as
 # images, and we have to sniff to see which to use.
 def fluxhelmrelease_containers(manifest):
+    def get_path(d, path):
+        try:
+            if "." in path:
+                key, rest = path.split(".", 1)
+                return get_path(d[key], rest)
+            else:
+                return d[path]
+        except KeyError:
+            return None
+
     def get_image(values):
         image = values['image']
         if isinstance(image, collections.Mapping) and 'repository' in image:
@@ -177,13 +221,34 @@ def fluxhelmrelease_containers(manifest):
             image = '%s:%s' % (image, values['tag'])
         return image
 
+    def get_image_for_mapping(values, mapping):
+        if 'repository' not in mapping:
+            return None
+        repository = get_path(values, v['repository'])
+        if not isinstance(repository, str) or repository == '':
+            return None
+        image = repository
+        if 'tag' in v:
+            tag = get_path(values, v['tag'])
+            if not isinstance(tag, str) or tag == '':
+                return None
+            image = '%s:%s' % (image, tag)
+        return image
+
+    def replace(d, value):
+        for i in range(len(d)):
+            if d[i]['name'] == value['name']:
+                d[i] = value
+                return
+        d.append(value)
+
     containers = []
     values = manifest['spec']['values']
     # Easiest one: the values section has a key called `image`, which
     # has the image used somewhere in the templates. Since we don't
     # know which container it appears in, it gets a standard name.
     if 'image' in values:
-        containers =  [{
+        containers = [{
             'name': FHR_CONTAINER,
             'image': get_image(values),
         }]
@@ -193,6 +258,13 @@ def fluxhelmrelease_containers(manifest):
     for k, v in mappings(values):
         if 'image' in v:
             containers.append({'name': k, 'image': get_image(v)})
+    # Third easiest: loop through found container mappings and attempt
+    # to construct containers from the collected dot notation paths.
+    for k, v in container_mappings_from_annotations(manifest).items():
+        image = get_image_for_mapping(values, v)
+        if not image:
+            continue
+        replace(containers, {'name': k, 'image': image})
     return containers
 
 def set_fluxhelmrelease_container(manifest, container, replace):
@@ -226,6 +298,17 @@ def set_fluxhelmrelease_container(manifest, container, replace):
             pass
         return reg, im, tag
 
+    def set_path(d, path, value):
+        keys = path.split(".")
+        for k in keys[:-1]:
+            if k not in d:
+                return False
+            d = d[k]
+        if not isinstance(d[keys[-1]], str):
+            return False
+        d[keys[-1]] = value
+        return True
+
     def set_image(values):
         image = values['image']
         imageKey = 'image'
@@ -249,10 +332,29 @@ def set_fluxhelmrelease_container(manifest, container, replace):
         else:
             values[imageKey] = replace
 
+    def set_image_for_mapping(values, mapping):
+        reg, im, tag = parse_ref()
+        if 'registry' in mapping and 'tag' in mapping:
+            set_path(values, mapping['registry'], reg)
+            set_path(values, mapping['repository'], im)
+            set_path(values, mapping['tag'], tag)
+        elif 'registry' in mapping:
+            set_path(values, mapping['registry'], reg)
+            set_path(values, mapping['repository'], '%s:%s' % (im, tag))
+        elif 'tag' in mapping:
+            set_path(values, mapping['repository'], im)
+            set_path(values, mapping['tag'], tag)
+        else:
+            set_path(values, mapping['repository'], replace)
+
     values = manifest['spec']['values']
     if container['name'] == FHR_CONTAINER and 'image' in values:
         set_image(values)
         return
+    for k, v in container_mappings_from_annotations(manifest).items():
+        if k == container['name'] and 'repository' in v:
+            set_image_for_mapping(values, v)
+            return
     for k, v in mappings(values):
         if k == container['name'] and 'image' in v:
             set_image(v)
